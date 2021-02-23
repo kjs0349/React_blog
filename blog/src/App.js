1758d0b732a30c8fae9a841442b9f1e967944198
Jisu <kjl0349@naver.com>
2021-02-23 11:12:54 +0900

    useState를 이용해 좋아요 기능 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '신림 맛집', '영등포 맛집']);
+  let [좋아요, 좋아요변경] = useState(0);
 
   let posts = '강남 고기 맛집';
 
@@ -13,7 +14,22 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={()=> {좋아요변경(좋아요+1);}}>👍</span> {좋아요} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[3] } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
