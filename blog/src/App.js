5aca6a4e9acd561df48001b39b93361c44a89416
Jisu <kjl0349@naver.com>
2021-02-26 22:44:57 +0900

    map에 i를 줘서 각각의 컴포넌트마다 다르게 동작하도록 설정, 입력값 state에 저장

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '신림 맛집', '영등포 맛집']);
-  let [좋아요, 좋아요변경] = useState(0);
+  let [좋아요, 좋아요변경] = useState([0,0,0,0]);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState('');
 
   let [modal, modal변경] = useState(false);
 
@@ -24,10 +27,14 @@ function App() {
       {/* <button onClick={ change }>버튼</button> */}
 
       {
-        글제목.map(function(글){
+        글제목.map(function(글, i){
           return(
             <div className="list">
-              <h3> { 글 } <span onClick={()=> {좋아요변경(좋아요+1);}}>👍</span> {좋아요}</h3>
+              <h3 onClick={()=>{누른제목변경(i); modal변경(true)}}> { 글 } <span onClick={()=> {
+                let newArray = [...좋아요];
+                newArray[i] = parseInt(newArray[i] + 1);
+                좋아요변경(newArray);
+              }}>👍</span> {좋아요[i]}</h3>
               <p>2월 17일 발행</p>
               <hr/>
             </div>
@@ -35,11 +42,12 @@ function App() {
         })
       }
 
+      <input onChange={(e)=>{입력값변경(e.target.value); console.log({입력값})}}/>
 
       <button onClick={()=>{modal변경(!modal)}}>버튼</button>
       {
         modal === true
-        ? <Modal 글제목={글제목}/>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}/>
         : null
       }
       
@@ -51,7 +59,7 @@ function App() {
 function Modal(props) {
   return(
     <div className="modal">
-      <h2>{props.글제목[0]}</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
