8a18bda4d6e79d938357e143d42fb98ab0ef2bf7
Jisu <kjl0349@naver.com>
2021-02-26 14:31:23 +0900

    spread 연산자를 사용해서 복사 후 재할당

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,12 +8,18 @@ function App() {
 
   let posts = '강남 고기 맛집';
 
+  function change() {
+    let newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={()=>{글제목변경(['여자 코트 추천', '강남 우동 맛집', '신림 맛집', '영등포 맛집']);}}>버튼</button>
+      <button onClick={ change }>버튼</button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={()=> {좋아요변경(좋아요+1);}}>👍</span> {좋아요} </h3>
         <p>2월 17일 발행</p>
